78e8fc8b7b19729682c85447184fe61e61e93184
Akpevwe11 <essiakpevwepraise@gmail.com>
2020-09-28 15:02:59 +0100

    continue

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 try {
 
 $pdo = new PDO('mysql:host=localhost; dbname=ijdb;
- charset=utf8', 'root', '123@Zuckerberg@12'); 
+ charset=utf8', 'root', ''); 
 
  $pdo->setAttribute(PDO::ATTR_ERRMODE, 
  PDO::ERRMODE_EXCEPTION); 
